11f8a68cac90e375380cbe442ec49cb4e8cc4f1a
miami78 <miamilarry78@gmail.com>
2019-09-30 15:22:36 +0300

    created homepage component and added sass styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './homepage.component';
+
 function App() {
   return (
-    <div >Crown-clothing</div>
+    <div >
+      <HomePage />
+    </div>
   );
 }
 
