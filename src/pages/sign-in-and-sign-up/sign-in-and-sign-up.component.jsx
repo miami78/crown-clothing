3169a122a32c39d68dd670d13c464650f068ad22
Miami78 <miamilarry78@gmail.com>
2020-04-01 19:02:37 +0300

    add sign in styling

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component.jsx'
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUpPage = () => (
+    <div className='sign-in-and-sign-up'>SIGN IN
+    <SignIn />
+    </div>
+)
+
+export default SignInAndSignUpPage
