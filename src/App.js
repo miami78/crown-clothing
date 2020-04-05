3f82bda90802aed6a5bf29a253dbb79983c21b51
Miami78 <miamilarry78@gmail.com>
2020-04-05 09:38:09 +0300

    add user storage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
 import Header from './components/header/header.component.jsx';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -18,12 +18,12 @@ class App extends React.Component {
     };
   }
 
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
+
     componentDidMount() {
-      this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-        this.setState({ currentUser: user });
-        console.log(user);
-      })
+      this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+        createUserProfileDocument(user);
+      });
     }
   
     componentWillUnmount() {
